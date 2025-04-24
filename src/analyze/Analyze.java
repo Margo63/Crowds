92118ea740f26b437a0fda4a76e741c09dd09c55
Margo63 <margo-vinograd@mail.ru>
2025-04-24 17:08:45 +0300

    add frech

--- src/analyze/Analyze.java
+++ src/analyze/Analyze.java
@@ -78,6 +78,23 @@ public class Analyze implements IObserver {
     }
 
     private void analizeWay(){
+        double [][]matrix = new double[pedestrianWays.size()][pedestrianWays.size()];
+        for (int i = 0; i < pedestrianWays.size(); i++) {
+            for (int j = i+1; j < pedestrianWays.size(); j++) {
+                double distance = FrechetDistance.frechetDistance(
+                        pedestrianWays.get(i),
+                        pedestrianWays.get(j)
+                );
+                matrix[i][j] = distance;
+                matrix[j][i] = distance;
+            }
+        }
+        for (int i = 0; i < pedestrianWays.size(); i++) {
+            for (int j = 0; j < pedestrianWays.size(); j++) {
+                System.out.print(matrix[i][j]+"\t");
+            }
+            System.out.println();
+        }
 
     }
 
